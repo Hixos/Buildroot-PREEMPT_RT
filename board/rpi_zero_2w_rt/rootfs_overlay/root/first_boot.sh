b3c684ac9d289e952dcaa4bd1374cdb56fe91c0b
Luca Erbetta <luca.erbetta105@gmail.com>
2023-12-23 18:10:11 +0100

    Update rootfs overlay

--- board/rpi_zero_2w_rt/rootfs_overlay/root/first_boot.sh
+++ board/rpi_zero_2w_rt/rootfs_overlay/root/first_boot.sh
@@ -5,7 +5,7 @@ set -e
 # Resize partition to fill available space
 echo "Resizing filesystem..."
 
-parted -s /dev/mmcblk0 resizepart 2 100%
+parted /dev/mmcblk0 resizepart 2 100%
 resize2fs /dev/mmcblk0p2
 
 # Install oh my zsh
@@ -16,14 +16,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Append default configuration to .zshrc
 echo "Appending .zshrc configuration..."
 
-cat <<EOT >> /root/.zshrc
-alias b="cd .."
-alias z="cd -"
-alias c="clear"
-
-function mkc() {
-	mkdir $1 && cd $1
-}
-EOT
+cat .zshrc_fragment >> .zshrc
+rm .zshrc_fragment
 
 echo "Done!"
